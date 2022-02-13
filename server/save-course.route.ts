c0d4fff6869eb3027d1abb08a35347598b7e4055
Vlad Li <vlali@softserveinc.com>
2022-02-13 13:09:44 +0200

    initial commit

---
+++ server/save-course.route.ts
@@ -0,0 +1,17 @@
+
+import {Request, Response} from 'express';
+import {findCourseById} from './db-data';
+
+export function saveCourse(req: Request, res: Response) {
+  const id = req.params['id'];
+  const changes = req.body;
+
+  console.log('Saving course', id, JSON.stringify(changes));
+
+  const course = findCourseById(Number(id));
+
+  course.titles = changes.titles;
+
+  res.status(200).json(course);
+}
+
